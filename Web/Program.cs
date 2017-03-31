8d716526aefce9105ed21cc6726a43fcec3ac274
Lev Lehn <lev.lehn@kardex.com>
2017-03-31 13:17:06 +0200

    initial commit

---
+++ Web/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+#pragma warning disable SA1600
+
+namespace Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
